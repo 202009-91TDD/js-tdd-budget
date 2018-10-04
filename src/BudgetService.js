ef829cd99288e6d99e94d23ce671edf161677448
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-10-04 14:17:09 +0800

    :recycle: introduce get budget

--- src/BudgetService.js
+++ src/BudgetService.js
@@ -2,25 +2,29 @@ const moment = require('moment')
 
 const queryBudget = (startDate, endDate, budgets) => {
     if (startDate.format('YYYYMM') === endDate.format('YYYYMM')) {
-        return getDailyAmount(budgets, startDate) * getDayCount(startDate, endDate)
+        return getDailyAmount(getBudget(budgets, startDate)) * getDayCount(startDate, endDate)
     } else {
         let sumBudget = 0
-        sumBudget += getDailyAmount(budgets, startDate) * getDayCount(startDate, moment(startDate).endOf('month'))
-        sumBudget += getDailyAmount(budgets, endDate) * getDayCount(moment(endDate).startOf('month'), endDate)
+        sumBudget += getDailyAmount(getBudget(budgets, startDate)) * getDayCount(startDate, moment(startDate).endOf('month'))
+        sumBudget += getDailyAmount(getBudget(budgets, endDate)) * getDayCount(moment(endDate).startOf('month'), endDate)
         for (let m = moment(startDate).add(1, 'month'); m.isBefore(moment(endDate).add(-1, 'month')); m.add(1, 'month')) {
-            sumBudget += getDailyAmount(budgets, m) * getDayCount(moment(m).startOf('month'), moment(m).endOf('month'))
+            sumBudget += getDailyAmount(getBudget(budgets, m)) * getDayCount(moment(m).startOf('month'), moment(m).endOf('month'))
         }
         return sumBudget
     }
 }
 
-const getDailyAmount = (budgets, dateInBudget) => {
-    const budget = budgets[dateInBudget.format('YYYYMM')];
-    if (budget) {
-        return budget / dateInBudget.daysInMonth()
+const getBudget = (budgets, dateInBudget) => {
+    const amount = budgets[dateInBudget.format('YYYYMM')];
+    if (amount) {
+        return { amount, month: dateInBudget}
     } else {
-        return 0
+        return { amount: 0, month: dateInBudget}
     }
+}
+
+const getDailyAmount = (budget) => {
+    return budget.amount / budget.month.daysInMonth()
 }
 
 const getDayCount = (startDate, endDate) => endDate.diff(startDate, 'days') + 1
