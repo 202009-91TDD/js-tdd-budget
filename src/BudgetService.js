f2a4a18df1eebc7fbd3d7ff64dd52a131ef6297b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-10-04 15:11:35 +0800

    :recycle: remove unneeded code of calculating first and last budget

--- src/BudgetService.js
+++ src/BudgetService.js
@@ -2,30 +2,21 @@ const moment = require('moment')
 
 const queryBudget = (startDate, endDate, budgets) => {
     const period = {startDate, endDate};
-    if (startDate.format('YYYYMM') === endDate.format('YYYYMM')) {
-        const budget = getBudget(budgets, startDate);
-        return getDailyAmount(budget) * getOverlappingDayCount(period, budget.period)
-    } else {
-        let sumBudget = 0
+    let sumBudget = 0
 
-        const firstBudget = getBudget(budgets, startDate);
-        sumBudget += getDailyAmount(firstBudget) * getOverlappingDayCount(period, firstBudget.period)
-        const lastBudget = getBudget(budgets, endDate);
-        sumBudget += getDailyAmount(lastBudget) * getOverlappingDayCount(period, lastBudget.period)
-        for (let m = moment(startDate).add(1, 'month'); m.isBefore(moment(endDate).add(-1, 'month')); m.add(1, 'month')) {
-            const budget = getBudget(budgets, m);
-            sumBudget += getDailyAmount(budget) * getOverlappingDayCount(period, budget.period)
-        }
-        return sumBudget
+    for (let m = moment(startDate).startOf('month'); m.isSameOrBefore(moment(endDate)); m.add(1, 'month')) {
+        const budget = getBudget(budgets, m);
+        sumBudget += getDailyAmount(budget) * getOverlappingDayCount(period, budget.period)
     }
+    return sumBudget
 }
 
 const getBudget = (budgets, dateInBudget) => {
     const amount = budgets[dateInBudget.format('YYYYMM')];
-    if (amount) {
-        return {amount, month: dateInBudget, period: {startDate: moment(dateInBudget).startOf('month'), endDate: moment(dateInBudget).endOf('month')}}
-    } else {
-        return {amount: 0, month: dateInBudget,  period: {startDate: moment(dateInBudget).startOf('month'), endDate: moment(dateInBudget).endOf('month')}}
+    return {
+        amount: amount ? amount : 0,
+        month: dateInBudget,
+        period: {startDate: moment(dateInBudget).startOf('month'), endDate: moment(dateInBudget).endOf('month')}
     }
 }
 
@@ -35,7 +26,7 @@ const getDayCount = period => period.endDate.diff(period.startDate, 'days') + 1
 
 const getOverlappingDayCount = function (period, another) {
     const overlappingEndDate = period.endDate.isBefore(another.endDate) ? period.endDate : another.endDate;
-    const overlappingStartDate = period.startDate.isAfter(another.startDate) ? period.startDate: another.startDate;
+    const overlappingStartDate = period.startDate.isAfter(another.startDate) ? period.startDate : another.startDate;
     if (overlappingStartDate.isAfter(overlappingEndDate))
         return 0
     return getDayCount({startDate: overlappingStartDate, endDate: overlappingEndDate});
