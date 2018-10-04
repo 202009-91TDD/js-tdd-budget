a672c391cbd6d8aa160ecd6c74544a0fa8711d9f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-10-04 14:31:04 +0800

    :recycle: introduce period

--- src/BudgetService.js
+++ src/BudgetService.js
@@ -1,18 +1,19 @@
 const moment = require('moment')
 
 const queryBudget = (startDate, endDate, budgets) => {
+    const period = {startDate, endDate};
     if (startDate.format('YYYYMM') === endDate.format('YYYYMM')) {
-        return getDailyAmount(getBudget(budgets, startDate)) * getDayCount(startDate, endDate)
+        return getDailyAmount(getBudget(budgets, startDate)) * getDayCount(period)
     } else {
         let sumBudget = 0
 
         const firstBudget = getBudget(budgets, startDate);
-        sumBudget += getDailyAmount(firstBudget) * getDayCount(startDate, getEndDate(firstBudget))
+        sumBudget += getDailyAmount(firstBudget) * getDayCount({startDate: period.startDate, endDate: getEndDate(firstBudget)})
         const lastBudget = getBudget(budgets, endDate);
-        sumBudget += getDailyAmount(lastBudget) * getDayCount(getStartDate(lastBudget), endDate)
+        sumBudget += getDailyAmount(lastBudget) * getDayCount({startDate: getStartDate(lastBudget), endDate: period.endDate})
         for (let m = moment(startDate).add(1, 'month'); m.isBefore(moment(endDate).add(-1, 'month')); m.add(1, 'month')) {
             const budget = getBudget(budgets, m);
-            sumBudget += getDailyAmount(budget) * getDayCount(getStartDate(budget), getEndDate(budget))
+            sumBudget += getDailyAmount(budget) * getDayCount({startDate: getStartDate(budget), endDate: getEndDate(budget)})
         }
         return sumBudget
     }
@@ -21,15 +22,15 @@ const queryBudget = (startDate, endDate, budgets) => {
 const getBudget = (budgets, dateInBudget) => {
     const amount = budgets[dateInBudget.format('YYYYMM')];
     if (amount) {
-        return { amount, month: dateInBudget}
+        return {amount, month: dateInBudget}
     } else {
-        return { amount: 0, month: dateInBudget}
+        return {amount: 0, month: dateInBudget}
     }
 }
 
 const getDailyAmount = budget => budget.amount / budget.month.daysInMonth()
 
-const getDayCount = (startDate, endDate) => endDate.diff(startDate, 'days') + 1
+const getDayCount = period => period.endDate.diff(period.startDate, 'days') + 1
 
 const getEndDate = budget => moment(budget.month).endOf('month')
 
