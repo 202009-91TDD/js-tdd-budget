dc625c5ae4ba5cd7182abb44fabdbe24c4c45e4b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-10-04 11:08:44 +0800

    :recycle: replace month string with date

--- src/BudgetService.js
+++ src/BudgetService.js
@@ -4,15 +4,13 @@ const queryBudget = (startDate, endDate, budgets) => {
 
     const diffDate = endDate.diff(startDate, 'days') + 1
 
-    const startMonth = startDate.format('YYYYMM')
-    const endMonth = endDate.format('YYYYMM')
-    if (startMonth === endMonth) {
-        return getDailyBudget(budgets, startMonth) * diffDate
+    if (startDate.format('YYYYMM') === endDate.format('YYYYMM')) {
+        return getDailyAmount(budgets, startDate) * diffDate
     } else {
         let sumBudget = 0
         const totalStartLeftDays = moment(startDate).endOf('month').diff(startDate, 'Days') + 1
-        sumBudget += getDailyBudget(budgets, startMonth) * totalStartLeftDays
-        sumBudget += getDailyBudget(budgets, endMonth) * endDate.date()
+        sumBudget += getDailyAmount(budgets, startDate) * totalStartLeftDays
+        sumBudget += getDailyAmount(budgets, endDate) * endDate.date()
         if (endDate.diff(startDate, 'months') >= 2) {
             for (let m = moment(startDate).add(1, 'month'); m.isBefore(moment(endDate).add(-1, 'month')); m.add(1, 'month')) {
                 sumBudget += budgets[moment(m).format('YYYYMM')] ? budgets[moment(m).format('YYYYMM')] : 0
@@ -22,12 +20,13 @@ const queryBudget = (startDate, endDate, budgets) => {
     }
 }
 
-const getDailyBudget = (budgets, monthString) => {
-    if (!budgets[monthString]) {
+const getDailyAmount = (budgets, dateInBudget) => {
+    const budget = budgets[dateInBudget.format('YYYYMM')];
+    if (budget) {
+        return budget / dateInBudget.daysInMonth()
+    } else {
         return 0
     }
-    const lengthOfMonth = moment(monthString, 'YYYYMM').daysInMonth()
-    return budgets[monthString] / lengthOfMonth
 }
 
 module.exports = queryBudget
